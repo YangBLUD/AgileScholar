22bead04a2d585a7d8fcf39b3c74a290d054f240
YangBLUD <89242017+YangBLUD@users.noreply.github.com>
2023-11-14 21:10:48 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import {useRouter} from 'vue-router'
+const router=useRouter();
+// router.push('/login')
+</script>
+
+<template>
+  <router-view></router-view>
+</template>
+
+<style scoped>
+</style>
