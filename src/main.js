31d02fdeb22d0dd66eaed1960c872d75f56c7569
YangBLUD <89242017+YangBLUD@users.noreply.github.com>
2023-11-14 21:11:12 +0800

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router/index.js'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import store from "./store/index.js"
+const app=createApp(App)
+app.use(router)
+app.use(ElementPlus)
+app.use(store)
+app.mount('#app')
