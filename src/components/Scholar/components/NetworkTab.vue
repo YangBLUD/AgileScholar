a7904d37517f7a1f6e5f7131d688ebdf821abc20
Lvchaoyue <1417743380@qq.com>
2023-11-29 00:16:12 +0800

    feat(scholar): add scholar papaer page

--- src/components/Scholar/components/NetworkTab.vue
+++ src/components/Scholar/components/NetworkTab.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>
-    <p>这里可以展示学者的关系网络图。</p>
-    <!-- 可以使用专门的库来渲染网络图，如ECharts, D3.js等 -->
-  </div>
+  <el-container class="network-container">
+    <el-aside width="200px">
+      <el-menu>
+        <el-menu-item index="1" @click="currentView = 'cited'">被引关系</el-menu-item>
+        <el-menu-item index="1" @click="currentView = 'partnership'">合作关系</el-menu-item>
+        <el-menu-item index="2" @click="currentView = 'reference'">引用关系</el-menu-item>
+      </el-menu>
+    </el-aside>
+    <el-main>
+      <component :is="currentComponent"></component>
+    </el-main>
+  </el-container>
 </template>
 
 <script setup>
-// 导入网络图渲染所需的库
+import {computed, ref} from "vue";
+import Cited from "./Network-components/Cited.vue"
+import Partnership from "./Network-components/Partnership.vue"
+import Reference from "./Network-components/Reference.vue"
+const componentsMap = {
+  partnership: Partnership,
+  cited:Cited,
+  reference:Reference
+};
+const currentView = ref('cited');
+const currentComponent = computed(() => componentsMap[currentView.value]);
+
 </script>
--- src/components/Scholar/components/PapersTab.vue
+++ src/components/Scholar/components/PapersTab.vue
@@ -1,24 +1,78 @@
+
 <template>
-  <div>
-    <el-table :data="papers" style="width: 100%">
-      <el-table-column prop="title" label="论文标题"></el-table-column>
-      <el-table-column prop="authors" label="作者"></el-table-column>
-      <el-table-column prop="journal" label="期刊"></el-table-column>
-      <el-table-column prop="year" label="年份"></el-table-column>
-      <el-table-column prop="download" label="下载量"></el-table-column>
-      <el-table-column prop="quote" label="引用"></el-table-column>
-    </el-table>
-  </div>
+  <el-row>
+    <el-col :span="6">
+      <el-menu default-active="1" class="filter-menu" accordion>
+          <el-menu-item-group>
+            <el-menu-item index="1-1" @click="filterBy('type', '全部')">全部</el-menu-item>
+            <el-menu-item index="1-1" @click="filterBy('type', '期刊')">期刊</el-menu-item>
+            <el-menu-item index="1-2" @click="filterBy('type', '学位')">学位</el-menu-item>
+            <el-menu-item index="1-3" @click="filterBy('type', '会议')">会议</el-menu-item>
+            <el-menu-item index="1-4" @click="filterBy('type', '成果')">成果</el-menu-item>
+            <el-menu-item index="1-5" @click="filterBy('type', '专利')">专利</el-menu-item>
+            <el-menu-item index="1-6" @click="filterBy('type', '报纸')">报纸</el-menu-item>
+            <el-menu-item index="1-6" @click="filterBy('type', '图书')">图书</el-menu-item>
+          </el-menu-item-group>
+      </el-menu>
+    </el-col>
+    <el-col :span="18">
+      <el-table :data="filteredPapers" style="width: 100%">
+        <el-table-column prop="title" label="论文标题"></el-table-column>
+        <el-table-column prop="authors" label="作者"></el-table-column>
+        <el-table-column prop="journal" label="期刊"></el-table-column>
+        <el-table-column prop="year" label="年份"></el-table-column>
+        <el-table-column prop="download" label="下载量"></el-table-column>-->
+        <el-table-column prop="quote" label="引用"></el-table-column>
+      </el-table>
+    </el-col>
+  </el-row>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
-// 假设的论文数据
+// 初始论文数据
 const papers = ref([
-  { title: '论文1', authors: '作者1', journal: '期刊1', year: '2023',download: 1000,quote: 200},
-  { title: '论文1', authors: '作者1', journal: '期刊1', year: '2023',download: 1000,quote: 200},
-  { title: '论文1', authors: '作者1', journal: '期刊1', year: '2023',download: 1000,quote: 200},
+  { title: '论文1', authors: '作者1', journal: '期刊1', year: '2023', type: '期刊', download: '100', quote: '20' },
+  { title: '论文2', authors: '作者2', journal: '期刊2', year: '2022', type: '学位', download: '200', quote: '10' },
+  { title: '论文3', authors: '作者3', journal: '期刊3', year: '2021', type: '会议', download: '300', quote: '30' },
+  { title: '论文4', authors: '作者4', journal: '期刊4', year: '2020', type: '成果', download: '150', quote: '25' },
+  { title: '论文5', authors: '作者5', journal: '期刊5', year: '2019', type: '专利', download: '350', quote: '15' },
+  { title: '论文6', authors: '作者6', journal: '期刊6', year: '2018', type: '报纸', download: '250', quote: '35' },
+  { title: '论文7', authors: '作者7', journal: '期刊7', year: '2017', type: '图书', download: '400', quote: '40' },
+  { title: '论文8', authors: '作者8', journal: '期刊8', year: '2023', type: '期刊', download: '120', quote: '50' },
+  { title: '论文9', authors: '作者9', journal: '期刊9', year: '2022', type: '学位', download: '220', quote: '60' },
+  { title: '论文10', authors: '作者10', journal: '期刊10', year: '2021', type: '会议', download: '320', quote: '70' },
+  { title: '论文11', authors: '作者1', journal: '期刊1', year: '2023', type: '期刊', download: '100', quote: '20' },
+  { title: '论文12', authors: '作者2', journal: '期刊2', year: '2022', type: '学位', download: '200', quote: '10' },
+  { title: '论文13', authors: '作者3', journal: '期刊3', year: '2021', type: '会议', download: '300', quote: '30' },
+  { title: '论文14', authors: '作者4', journal: '期刊4', year: '2020', type: '成果', download: '150', quote: '25' },
+  { title: '论文15', authors: '作者5', journal: '期刊5', year: '2019', type: '专利', download: '350', quote: '15' },
+  { title: '论文16', authors: '作者6', journal: '期刊6', year: '2018', type: '报纸', download: '250', quote: '35' },
+  { title: '论文17', authors: '作者7', journal: '期刊7', year: '2017', type: '图书', download: '400', quote: '40' },
+  { title: '论文18', authors: '作者8', journal: '期刊8', year: '2023', type: '期刊', download: '120', quote: '50' },
+  { title: '论文19', authors: '作者9', journal: '期刊9', year: '2022', type: '学位', download: '220', quote: '60' },
+  { title: '论文20', authors: '作者10', journal: '期刊10', year: '2021', type: '会议', download: '320', quote: '70' },
   // 更多论文数据...
 ]);
+
+
+// 筛选后的论文数据
+const filteredPapers = ref([...papers.value]);
+
+// 筛选函数
+function filterBy(field, value) {
+  if (value === '全部') {
+    filteredPapers.value = papers.value;
+  } else {
+    filteredPapers.value = papers.value.filter(paper => paper[field] === value);
+  }
+}
 </script>
+
+<style>
+.filter-menu {
+  width: 100%;
+  border-right: 1px solid #ebeef5;
+}
+</style>
