8ca5bcffc823cde4bf460dce1af936911ee65a9c
YangBLUD <2433171628@qq.com>
2023-12-25 22:37:28 +0800

    test

--- src/components/searchResult/scholars.vue
+++ src/components/searchResult/scholars.vue
@@ -1,33 +1,34 @@
 <template>
     <div class="main">
         <div class="pic">
-            <img src="../../assets/logo.png" class="scholar-pic"/>
+            <img src="../../assets/logo.png" class="scholar-pic" />
         </div>
         <div class="name" style="text-align: center;">
-<!--            <router-link :to="{name:'scholar-display', params:{id:props.info.id} }">-->
-<!--                <span v-html="props.info.display_name" style="display: inline-block;"></span>-->
-<!--            </router-link>-->
+            <router-link :to="{ name: 'scholar-display', params: { id: props.info.id } }">
+                <span v-html="props.info.display_name" style="display: inline-block;"></span>
+            </router-link>
         </div>
-        <div class="jigou" v-if="props.info.institution && props.info.institution.length!=0" style="text-align: center;"> 
-            <router-link :to="{name:'institution-display', params:{id:props.info.institution[0].id} }">
+        <div class="jigou" v-if="props.info.institution && props.info.institution.length != 0" style="text-align: center;">
+            <router-link :to="{ name: 'institution-display', params: { id: props.info.institution[0].id } }">
                 <span v-html="props.info.institution[0].name" style="display: inline-block;"></span>
             </router-link>
         </div>
         <div class="star">
-            <stardialog :token = "props.token" :paper_id="props.info.id" :type="type" :is_star="props.info.is_star"></stardialog>
+            <stardialog :token="props.token" :paper_id="props.info.id" :type="type" :is_star="props.info.is_star">
+            </stardialog>
         </div>
-        
+
     </div>
 </template>
 <script setup>
 import { defineProps } from 'vue';
-import { reactive, ref, onMounted, onUnmounted ,onBeforeMount} from "vue";
+import { reactive, ref, onMounted, onUnmounted, onBeforeMount } from "vue";
 import axios from "axios";
 import stardialog from "./stardialog.vue";
 import { useStore } from "vuex";
 const Store = useStore();
 onMounted(() => {
-    type.value = 1;    
+    type.value = 1;
 });
 //用于收藏
 const paper_id = ref("2106749358");
@@ -36,40 +37,45 @@ const folder_id = ref(2);
 const folderlist = ref([]);
 
 const props = defineProps({
-    info:Object,
-    token:String,
+    info: Object,
+    token: String,
 })
 </script>
 <style scoped>
-.main{
+.main {
     display: flex;
     width: 100%;
     height: 100%;
     cursor: pointer;
 }
-.pic{
+
+.pic {
     width: 250px;
     height: 140px;
-    .scholar-pic{
+
+    .scholar-pic {
         margin-left: 60%;
-        margin-top:20% ;
+        margin-top: 20%;
         width: 110px;
-        height:110px;
+        height: 110px;
         border-radius: 50%;
     }
 }
-.name{
+
+.name {
     position: relative;
     left: -40px;
     top: 150px;
     height: 20px;
     width: 600px;
-    >>> em {
+
+    >>>em {
         background-color: yellow;
     }
 }
-.name span{
-    display:inline-block;
+
+.name span {
+    display: inline-block;
     overflow: hidden;
     white-space: nowrap;
     text-overflow: ellipsis;
@@ -77,7 +83,8 @@ const props = defineProps({
     left: -50px;
     width: 200px;
 }
-.jigou{
+
+.jigou {
     color: #0077c2;
     font-size: .875rem;
     position: relative;
@@ -88,12 +95,14 @@ const props = defineProps({
     /* white-space: pre-wrap; */
     /* overflow: hidden; */
     text-overflow: ellipsis;
-    >>> em {
+
+    >>>em {
         background-color: yellow;
     }
 }
-.jigou span{
-    display:inline-block;
+
+.jigou span {
+    display: inline-block;
     overflow: hidden;
     white-space: nowrap;
     text-overflow: ellipsis;
@@ -101,7 +110,8 @@ const props = defineProps({
     left: -50px;
     width: 200px;
 }
-.star{
+
+.star {
     position: relative;
     left: -300px;
     top: 20px;
@@ -112,11 +122,10 @@ const props = defineProps({
     height: 30px;
     width: 33px;
     display: flex;
-    justify-content:center;
-    align-items:center;
-}
-.star:hover{
-    background-color: #d7d7d7;
+    justify-content: center;
+    align-items: center;
 }
 
-</style>
+.star:hover {
+    background-color: #d7d7d7;
+}</style>
